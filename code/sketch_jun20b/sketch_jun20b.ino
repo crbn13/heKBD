bd6982aee80e19b91e8cab5e4753931c87f74754
rory <roryredking8@gmail.com>
2025-06-21 00:14:55 +0100

    Working code

---
+++ code/sketch_jun20b/sketch_jun20b.ino
@@ -0,0 +1,303 @@
+#include "Adafruit_TinyUSB.h"
+
+// Number of keyboard keys
+#define KEY_COUNT 2
+
+
+// Analogue pins
+#define ADC1 A0
+#define ADC2 A1
+typedef uint8_t NORMALISED_ADC_VAL;
+#define MAX_NORMALISED_ADC_VAL 255
+
+#define MAX_ANALOG_VALUE 4096
+#define MIN_ANALOG_VALUE 0
+
+uint8_t const desc_hid_report[] = {
+  TUD_HID_REPORT_DESC_KEYBOARD()
+};
+
+// HID STUFF :
+Adafruit_USBD_HID usb_hid;
+
+
+long start = 0;
+long end = 100;  // just in case div0 happens
+float hz = 0;
+
+
+
+struct key {
+  uint8_t normalised;  // number between 0-255 where 0 is unpressed and 255 is fully depressed
+  uint16_t real;       // the value from ADC
+  float min_real;      // minimum value read from ADC
+  float max_real;      // value from ADC analog to digital conversion
+  float factor;        // a number to map the un normalised values to 0-255 num
+  bool active_state;   // The state that was last sent over usb
+  uint8_t keycode;     // the HID_KEY value of the specific switch
+
+  key()
+    : normalised(0), real(0), min_real(MAX_ANALOG_VALUE), max_real(MIN_ANALOG_VALUE), factor(0.0F), active_state(0), keycode(0) {
+  }
+};
+
+key keys[KEY_COUNT];
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+
+
+  if (!TinyUSBDevice.isInitialized()) {
+    TinyUSBDevice.begin(0);
+  }
+
+  // Setup HID
+  usb_hid.setBootProtocol(HID_ITF_PROTOCOL_KEYBOARD);
+  usb_hid.setPollInterval(1);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+  usb_hid.setStringDescriptor("TinyUSB Keyboard");
+
+  // Set up output report (on control endpoint) for Capslock indicator
+  usb_hid.setReportCallback(NULL, hid_report_callback);
+
+  usb_hid.begin();
+
+  // If already enumerated, additional class driverr begin() e.g msc, hid, midi won't take effect until re-enumeration
+  if (TinyUSBDevice.mounted()) {
+    TinyUSBDevice.detach();
+    delay(10);
+    TinyUSBDevice.attach();
+  }
+
+  // led pin
+#ifdef LED_BUILTIN
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+#endif
+
+  // set keycode values ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // need to actually make the custom keymap
+
+  // for ( int i = 0 ; i < KEY_COUNT ; i++)
+  // {
+  // keys[i].keycode = keymap.key[]
+  // }
+
+  keys[0].keycode = HID_KEY_D;
+  keys[1].keycode = HID_KEY_F;
+
+  // Find old min max values ~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  keys[0].real = analogRead(ADC1);
+  keys[1].real = analogRead(ADC2);
+
+  // Set min vals : 
+  for (int i = 0; i < KEY_COUNT; i++) {
+    keys[i].min_real = keys[i].real; // set the min value to the value at startup
+  }
+  // need to implement them being saved ofc
+
+}  // end of setup
+
+
+const int cycles = 1;
+
+void loop() {
+  start = micros();  // get time?
+  delay(1);
+
+#ifdef TINYUSB_NEED_POLLING_TASK
+  // Manual call tud_task since it isn't called by Core's background
+  TinyUSBDevice.task();
+#endif
+
+  // not enumerated()/mounted() yet: nothing to do
+  if (!TinyUSBDevice.mounted()) {
+    return;
+  }
+
+  for (int i = 0; i < cycles; i++) {
+    // int val = analogRead(ADC1);
+    // val = analogRead(ADC2);
+
+    Serial.print(keys[0].real);
+    Serial.print("  ");
+    Serial.print(keys[0].normalised);
+    Serial.print("  ");
+    Serial.print(keys[0].factor);
+    Serial.print("  ");
+    Serial.print(keys[0].max_real);
+    Serial.print("  ");
+    Serial.print(keys[0].min_real);
+    Serial.print("  ");
+    Serial.print(keys[0].active_state);
+
+    Serial.print("\t|  ");
+    Serial.print(analogRead(ADC2));
+    Serial.print("  ");
+    Serial.print(keys[1].normalised);
+    Serial.print("  ");
+    Serial.print(keys[1].factor);
+    Serial.print("\t| HZ = ");
+    Serial.println(hz);
+
+    process_hid();
+  }
+
+  end = micros();
+  // micros() is in micro seconds or E-6 of 1 second
+
+  hz = 1.0f / (float(end - start) / (1000000.0F * float(cycles)));
+  // ~~ Speeds notes : ~~ standard clock speed
+  // when just doing the 2 analog reads it runs at : 116,813.56 hz
+  // When printing each analog value to serial : 9,000 hz
+
+
+  // Serial.print("\t| HZ = ");
+  // Serial.println(hz);
+}
+
+
+
+void process_hid() {
+  // used to avoid send multiple consecutive zero report for keyboard
+  static bool keyPressedPreviously = false;
+
+  uint8_t count = 0;            // the number of keys being pressed
+  uint8_t keycodes[6] = { 0 };  // array of 6 keys that are being pressed
+  bool modifier_changed = false;
+  
+  keys[0].real = analogRead(ADC1);
+  keys[1].real = analogRead(ADC2);
+
+
+  // check active keys and assign to keycode
+  for (int i = 0; i < KEY_COUNT; i++) {
+    // Multiplexer code to set value would be here but it doesnt exist yet
+    modifier_changed = false;
+
+    if (keys[i].max_real < keys[i].real) {
+      keys[i].max_real = keys[i].real;
+      modifier_changed = true;
+    }
+
+    auto change_modifier = [&] ( ) {
+      if (keys[i].max_real != keys[i].min_real && keys[i].max_real - keys[i].min_real > 50)  // only set modifier if
+        keys[i].factor = (float)(NORMALISED_ADC_VAL)(-1) / (keys[i].max_real - keys[i].min_real);
+      modifier_changed = false;
+    };
+
+    if (modifier_changed) {
+      change_modifier();
+    }
+
+    decltype(keys[i].normalised) previous = +keys[i].normalised;
+
+    keys[i].normalised = +int(abs((+keys[i].real - keys[i].min_real) * keys[i].factor));
+
+    if (+int(abs((+keys[i].real - keys[i].min_real) * keys[i].factor)) > MAX_NORMALISED_ADC_VAL) // no cheeky buffer overflows
+      keys[i].normalised = MAX_NORMALISED_ADC_VAL;
+
+    // rapid trigger is by standard :
+
+    const int bounds_checker = 10;  // needs renaming, it accounts for the random variance in the analogue input
+    const int change_buffer = 2;
+
+    // 0 IS NOT DEPRESSED, 255 is FULLY DEPRESSED
+    if (keys[i].active_state)
+    {
+      if ( keys[i].normalised < bounds_checker && keys[i].normalised < +previous ) // check if unpressed and key at top
+      {
+        keys[i].active_state = false;
+        change_modifier();
+      }
+      else if (keys[i].normalised < +previous - change_buffer && keys[i].normalised < MAX_NORMALISED_ADC_VAL - bounds_checker) // check if direction of keystroke changed in middle of stroke
+      {
+        keys[i].active_state = false;
+        keycodes[count++] = keys[i].keycode;
+      }
+      else if (keys[i].normalised < MAX_NORMALISED_ADC_VAL - bounds_checker) // 
+      {
+        keycodes[count++] = keys[i].keycode;
+      }
+      else // if neither then set value to previous and switch still pressed
+      {
+        keycodes[count++] = keys[i].keycode;
+        keys[i].normalised = previous;
+      }
+    } 
+    else // if key not already pressed
+    {
+      if (keys[i].normalised > +previous + change_buffer && keys[i].normalised > bounds_checker)  // check that it has moved enough to achuate a press downwards
+      {
+        keys[i].active_state = true;
+        keycodes[count++] = keys[i].keycode;
+      } 
+      else if ( keys[i].normalised > previous) // if the keystroke is going downwards then set the value to previous so that the distance gets bigger if it keeps going down
+      {
+        keys[i].normalised = previous;
+      }
+      else // when keys.normalised is < previous the key is travelling upwards
+      {
+
+      }
+    }
+
+
+
+    if (count > 5)  // usb hid has a max report of 6 keys at a time :(
+      break;        // break out of loop
+  }
+
+
+  if (TinyUSBDevice.suspended() && count) {
+    // Wake up host if we are in suspend mode
+    // and REMOTE_WAKEUP feature is enabled by host
+    TinyUSBDevice.remoteWakeup();
+  }
+
+  // skip if hid is not ready e.g still transferring previous report
+  if (!usb_hid.ready()) return;
+
+  if (count) {
+    // Send report if there is key pressed
+    uint8_t const report_id = 0;
+    uint8_t const modifier = 0;  // modifier keys stored in array of 1 bit numbers
+
+    keyPressedPreviously = true;
+    usb_hid.keyboardReport(report_id, modifier, keycodes);
+  } else {
+    // Send All-zero report to indicate there is no keys pressed
+    // Most of the time, it is, though we don't need to send zero report
+    // every loop(), only a key is pressed in previous loop()
+    if (keyPressedPreviously) {
+      keyPressedPreviously = false;
+      usb_hid.keyboardRelease(0);
+    }
+  }
+}
+
+
+
+
+// Output report callback for LED indicator such as Caplocks
+void hid_report_callback(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+  (void)report_id;
+  (void)bufsize;
+
+  // LED indicator is output report with only 1 byte length
+  if (report_type != HID_REPORT_TYPE_OUTPUT) return;
+
+  // The LED bit map is as follows: (also defined by KEYBOARD_LED_* )
+  // Kana (4) | Compose (3) | ScrollLock (2) | CapsLock (1) | Numlock (0)
+  uint8_t ledIndicator = buffer[0];
+
+#ifdef LED_BUILTIN
+  // turn on LED if capslock is set
+  digitalWrite(LED_BUILTIN, ledIndicator & KEYBOARD_LED_CAPSLOCK);
+#endif
+}
