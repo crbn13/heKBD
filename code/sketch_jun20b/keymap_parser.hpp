3bcc20eb788485ec593c0ea79a7a1ae94f683d75
rory <rory@mail.com>
2025-08-22 23:35:24 +0100

    5 lines of header file

--- code/sketch_jun20b/keymap_parser.hpp
+++ code/sketch_jun20b/keymap_parser.hpp
@@ -1,5 +1,11 @@
 #ifndef GUARD_KEYMAP_PARSER_HPP
 #define GUARD_KEYMAP_PARSER_HPP
 
+#include "config.hpp"
+
+// Parses the keymap stored in the config file 
+bool parse_keymap();
+
+
 
 #endif // Include Guard
